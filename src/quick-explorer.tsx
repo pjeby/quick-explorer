3341ba0f0287686e034479e90a5de7cfe2a43cf8
PJ <pje@telecommunity.com>
2022-06-28 14:52:29 -0400

    Menu bug fixes for 0.15.3

--- src/quick-explorer.tsx
+++ src/quick-explorer.tsx
@@ -41,17 +41,19 @@ export default class QE extends Plugin {
         this.registerEvent(this.app.workspace.on("file-menu", (menu, file, source) => {
             let item: MenuItem
             if (source !== "quick-explorer") menu.addItem(i => {
-                i.setIcon("folder").setTitle("Show in Quick Explorer").onClick(e => { this.explorers.forWindow()?.browseFile(file); });
+                i.setIcon("folder").setTitle("Show in Quick Explorer").onClick(e => { this.explorers.forDom(item.dom)?.browseFile(file); });
                 item = i;
                 item.setSection?.("view");
             })
             if (item) {
                 const revealFile = i18next.t(`plugins.file-explorer.action-reveal-file`);
-                const idx = menu.items.findIndex(i => i.titleEl.textContent === revealFile);
-                // Remove this once 0.15.3+ is required
-                if (!menu.sections) (menu.dom as HTMLElement).insertBefore(item.dom, menu.items[idx+1].dom);
-                menu.items.remove(item);
-                menu.items.splice(idx+1, 0, item);
+                const idx = menu.items.findIndex(i => i.titleEl?.textContent === revealFile);
+                if (idx > -1) {
+                    // Remove this once 0.15.3+ is required
+                    if (!menu.sections) (menu.dom as HTMLElement).insertBefore(item.dom, menu.items[idx].dom);
+                    menu.items.remove(item);
+                    menu.items.splice(idx, 0, item);
+                }
             }
         }));
 
